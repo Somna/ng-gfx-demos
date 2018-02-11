bb0c665f69278547ab6d9f17070a785586390657
belkin <for.belkins@gmail.com>
2018-02-11 18:11:01 +0700

    fixes: a gsap types dependencies

--- src/app/animated-logo-gsap/animated-logo-gsap.component.ts
+++ src/app/animated-logo-gsap/animated-logo-gsap.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {Cubic, TweenMax} from "gsap";
 
 @Component({
   selector: 'app-animated-logo-gsap',
